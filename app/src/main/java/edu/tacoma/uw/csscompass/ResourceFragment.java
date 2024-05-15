18c26b33da9a1698faded97dbf75009bd549cc80
JJ Coldiron <jj.coldiron@outlook.com>
2024-05-14 20:37:51 -0700

    Redesigned Sprint 1 Content

---
+++ app/src/main/java/edu/tacoma/uw/csscompass/ResourceFragment.java
@@ -0,0 +1,19 @@
+package edu.tacoma.uw.csscompass;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResourceFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_resource, container, false);
+    }
+}
