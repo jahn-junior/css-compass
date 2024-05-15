ec0e1b3454e74cc0ade494eb5c545cae9126d569
JJ Coldiron <jj.coldiron@outlook.com>
2024-05-15 09:44:41 -0700

    Refactored and added button listeners in SetProgramFragment

--- app/src/main/java/edu/tacoma/uw/csscompass/SetProgramFragment.java
+++ app/src/main/java/edu/tacoma/uw/csscompass/SetProgramFragment.java
@@ -1,64 +1,66 @@
 package edu.tacoma.uw.csscompass;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SetProgramFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import edu.tacoma.uw.csscompass.databinding.FragmentHomeBinding;
+import edu.tacoma.uw.csscompass.databinding.FragmentSetProgramBinding;
+
 public class SetProgramFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SetProgramFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SetProgramFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SetProgramFragment newInstance(String param1, String param2) {
-        SetProgramFragment fragment = new SetProgramFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentSetProgramBinding mBinding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_program, container, false);
+        mBinding = FragmentSetProgramBinding.inflate(inflater, container, false);
+
+        return mBinding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBinding.bookNoelle.setOnClickListener(card -> {
+            Uri webpage = Uri.parse("https://apply.tacoma.uw.edu/portal/webinar");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
+
+        mBinding.bookFrancis.setOnClickListener(card -> {
+            Uri webpage = Uri.parse("https://outlook.office365.com/book/SETGraduateAdvisingFrancisNwagbara@cloud.washington.edu/");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
+
+        mBinding.bookBrandy.setOnClickListener(card -> {
+            Uri webpage = Uri.parse("https://outlook.office365.com/book/UWTSETAdvisingBrandyFeatherstone@cloud.washington.edu/");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
+
+        mBinding.bookChristian.setOnClickListener(card -> {
+            Uri webpage = Uri.parse("https://outlook.office365.com/book/UWTSETAdvisingChristianJames@cloud.washington.edu/");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
+
+        mBinding.bookBeth.setOnClickListener(card -> {
+            Uri webpage = Uri.parse("https://outlook.office365.com/book/UWTSETAdvisingBethJeffrey@cloud.washington.edu/");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(webIntent);
+        });
+    }
+
+
 }
